c6f8e78914cc291c564f45abafb290e3abea44bf
Guilherme Quartel <guibluis0811@gmail.com>
2025-05-21 10:22:34 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-6xl">Hello World</h1>
+@endsection
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Credit App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        @vite('resources/css/app.css')
+    </head>
+    <body class="">
+        @include('sections.header')
+
+        <main class="max-w-[1140px] w-[90%] mx-auto">
+            @yield('content')
+        </main>
+
+        @include('sections.footer')
+    </body>
+</html>
